a516af194ebf7d80a4dfa3b7c7d7f49b2dbbca77
gospodinyuriy3 <110244284+gospodinyuriy3@users.noreply.github.com>
2023-04-26 18:57:25 +0300

    reg

--- data/users.py
+++ data/users.py
@@ -2,12 +2,18 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 from flask_login import UserMixin
 
 
 class User(SqlAlchemyBase, UserMixin):
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
     __tablename__ = 'users'
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True,
